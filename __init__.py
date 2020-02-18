8e1fae598ccb2facbbe8cf8ebba7cde3d2f36300
Zacharopoulos Filippos <ziposc7@outlook.com>
2020-02-18 21:19:54 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+import gui_control
+
+def main():
+    # Start the GUI
+    gui_control.HombroApp().run()
+
+main()
---
+++ coms_control.py
@@ -0,0 +1,32 @@
+import paho.mqtt.client as mqtt
+
+broker_ip = '192.168.1.10'
+broker_port = 1883
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+ str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("/laptop")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client('Laptop')
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(broker_ip, broker_port, 60)
+client.publish('/pi', 'Successful Connection from client.')
+
+# Non blocking waiting for messages
+client.loop_start()
+
+def stop_coms():
+    print('Stopping communication with the broker.')
+    client.loop_stop()
+
+def send(device, action, payload):
+    client.publish(f'/pi/{device}/{action}', payload)
