6269006fdeaca904489e78703e5ed95b42c76d18
ZiposC <ziposc7@outlook.com>
2020-03-02 00:48:51 +0200

    Devices menu

--- GUI.py
+++ GUI.py
@@ -1,28 +1,31 @@
 import json
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.togglebutton import ToggleButton
-from kivy.properties import NumericProperty,ReferenceListProperty,ObjectProperty
+from kivy.properties import DictProperty, ListProperty,ObjectProperty
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from utilitywidgets import FloatInput, PresetButton
 import communications
 
-class HomeScreen(BoxLayout):
+class LedstripScreen(Screen):
 
     preset_btns = ObjectProperty()
-    preset_data = {}
+    preset_data = DictProperty()
 
-    def __init__(self, config, **kwargs):
+    def __init__(self, dev_data, **kwargs):
         super().__init__(**kwargs)
-
+        self.preset_btns.active_preset = dev_data['default_preset']
         # For each preset, add to the dictionary preset_params the parameters of the
         # preset and create the corresponding buttons.
-        self.preset_data = config['presets']
+        self.preset_data = dev_data['presets']
         for pr_name, params in self.preset_data.items():
             btn = PresetButton(text = pr_name, halign = 'center')
-            if pr_name == self.preset_btns.active_btn:
+            btn.bind(on_press = self.preset_clicked)
+            if self.preset_btns.active_preset == pr_name:
                 btn.state = 'down'
             self.preset_btns.add_widget(btn)
 
@@ -36,25 +39,46 @@ class HomeScreen(BoxLayout):
         communications.send(device.name, 'color', str(rgb_val))
 
     def preset_clicked(self, preset_btn):
-        name = preset_btn.text
         if (preset_btn.state == 'down'):
-            self.preset_btns.active_btn = name
+            name = preset_btn.text
+            self.preset_btns.active_preset = name
             print(name)
 
     def add_color_to_preset(self, rgb_val):
         # Adds the currently selected color to the active preset.
-        pr_name = self.preset_btns.active_btn
+        pr_name = self.preset_btns.active_preset
         self.preset_data[pr_name].append({'color': rgb_val, 'duration': 1})
 
         print(self.preset_data[pr_name][-1])
+
+class SettingsScreen(Screen):
+    pass
+
+
+class ScreenController(ScreenManager):
+    devices = ListProperty()
+
+    def __init__(self, config, **kwargs):
+        super().__init__(**kwargs)
+        self.devices = config['devices']
+
+    pass
+
 
 
 class HombroApp(App):
 
     def build(self):
         with open('config.json') as config_file:
-            config = json.load(config_file)
-        return HomeScreen(config)
+            config = json.load(config_file) # Get the configuration file
+        self.icon = 'icons/app_icon.png' # Set the logo of the app
+        self.scrn_ctrl = ScreenController(config)
+        for device in config['devices']:
+            # For each device create a screen and add it to the screen controller
+            if device['type'] == 'ledstrip':
+                self.scrn_ctrl.add_widget(LedstripScreen(device, name = device['name']))
+
+        return self.scrn_ctrl
 
     def on_stop(self):
         communications.stop_coms()
--- utilitywidgets.py
+++ utilitywidgets.py
@@ -1,15 +1,27 @@
 from kivy.uix.textinput import TextInput
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+from kivy.properties import ObjectProperty, ListProperty, StringProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from kivy.uix.modalview import ModalView
+from kivy.uix.label import Label
+
 import re
 
 class PresetButton(ToggleButton):
     pass
 
 
-class FloatInput(TextInput):
+class DeviceButton(ToggleButton):
+    pass
 
+
+class FloatInput(TextInput):
+# A widget that allows the user to input as text only float data.
     pat = re.compile('[^0-9]')
+
     def insert_text(self, substring, from_undo=False):
         pat = self.pat
         if '.' in self.text:
@@ -17,3 +29,31 @@ class FloatInput(TextInput):
         else:
             s = '.'.join([re.sub(pat, '', s) for s in substring.split('.', 1)])
         return super(FloatInput, self).insert_text(s, from_undo=from_undo)
+
+
+class Separator(Widget):
+    color = ListProperty()
+
+
+class OverlayView(ModalView):
+
+    device_btns = ObjectProperty()
+
+    def __init__(self, devices, **kwargs):
+        super().__init__(**kwargs)
+        for device in devices:
+            # For each device add an instance to the dropdown list
+            btn = DeviceButton(text = device['name'])
+            self.device_btns.add_widget(btn)
+
+
+class NavigationBar(BoxLayout):
+    overlay_btn = ObjectProperty()
+    devices = ListProperty()
+    selected_dev = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def on_devices(self, instance, value):
+        self.overlay = OverlayView(self.devices, attach_to = self.overlay_btn)
